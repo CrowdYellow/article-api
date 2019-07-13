d4460d1108ee155609316a44af6d2671c157434e
CrowdYellow <echo61546@gmail.com>
2019-07-13 19:05:28 +0800

    初始化基类

--- routes/api.php
+++ routes/api.php
@@ -2,22 +2,14 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', function($api) {
-    $api->get('version', function() {
-        return response('this is version v1');
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function($api) {
+    // 测试路由
+    $api->get('verificationCodes', function () {
+        return response('verificationCodes');
     });
 });
 
